7506d2864763c964ec909063b1d4f20d0e4b6890
syaifulnizamyahya <syaifulnizamyahya@gmail.com>
2017-12-14 13:23:13 +0800

    Adding a model

--- RazorPagesMovie/RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/RazorPagesMovie/Startup.cs
@@ -6,45 +6,53 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesMovie
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			// requires 
+			// using RazorPagesMovie.Models;
+			// using Microsoft.EntityFrameworkCore;
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
+			services.AddDbContext<MovieContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
+			services.AddMvc();
+		}
 
-            app.UseStaticFiles();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseBrowserLink();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+			}
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
-            });
-        }
-    }
+			app.UseStaticFiles();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller}/{action=Index}/{id?}");
+			});
+		}
+	}
 }
---
+++ RazorPagesMovie/RazorPagesMovie/readme.md
@@ -0,0 +1,10 @@
+﻿#### Db migration console command
+
+> Install-Package Microsoft.VisualStudio.Web.CodeGeneration.Design -Version 2.0.0
+>
+> Add-Migration Initial
+>
+> Update-Database
+
+#### Scaffolding command
+> dotnet aspnet-codegenerator razorpage -m Movie -dc MovieContext -udl -outDir Pages\Movies --referenceScriptLibraries
